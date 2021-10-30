15945579b242e2bec7aa0b625d6d2957f72d02a6
Denis Astahov <denis@astahov.net>
2021-10-29 17:27:01 -0700

    Recreate Resource - terraform apply -replace

---
+++ Lab-27-v0.15.2+/main.tf
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+#  Terraform - From Zero to Certified Professional
+#
+# Re-Create Resource in Terraform v0.15.2 and up
+#    1. terraform init
+#    2. terraform plan  -replace aws_instance.node2
+#    3. terraform apply -replace aws_instance.node2
+# Made by Denis Astahov
+#-------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "node1" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-1"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-2"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_instance" "node3" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-3"
+    Owner = "Denis Astahov"
+  }
+  depends_on = [aws_instance.node2]
+}
