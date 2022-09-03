09e567233afcd552ee6291082981c3f56a6e4dfe
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:32:17 -0700

    Build WebServer during Bootstrap with External TEMPLATE File

--- Lab-04/main.tf
+++ Lab-04/main.tf
@@ -10,6 +10,8 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 resource "aws_instance" "web" {
   ami                    = "ami-0c9bfc21ac5bf10eb" // Amazon Linux2
   instance_type          = "t3.micro"
@@ -29,6 +31,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web" {
   name        = "WebServer-SG"
   description = "Security Group for my WebServer"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   ingress {
     description = "Allow port HTTP"
