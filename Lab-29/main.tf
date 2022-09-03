62fc66011188397cf52ff6f490db414648a9d1f4
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:37:53 -0700

    Terraform Workspaces

--- Lab-29/main.tf
+++ Lab-29/main.tf
@@ -10,6 +10,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 data "aws_ami" "latest_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
@@ -42,6 +44,7 @@ EOF
 
 resource "aws_security_group" "web" {
   name_prefix = "WebServer SG Prod"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   ingress {
     from_port   = 80
