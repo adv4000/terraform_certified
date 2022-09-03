97afe2a46918f479abe3bf6ce71c6eee3fa5cfc6
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:36:31 -0700

    Use Variables and Multiply files

--- Lab-14/main.tf
+++ Lab-14/main.tf
@@ -18,6 +18,8 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 resource "aws_eip" "web" {
   instance = aws_instance.web.id
   tags     = merge(var.tags, { Name = "${var.tags["Environment"]}-EIP for WebServer Built by Terraform" })
@@ -39,6 +41,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web" {
   name        = "${var.tags["Environment"]}-WebServer-SG"
   description = "Security Group for my ${var.tags["Environment"]} WebServer"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   dynamic "ingress" {
     for_each = var.port_list
