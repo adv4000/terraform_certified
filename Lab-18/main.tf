796309790a540e092a5979a42a48add39c0ea264
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:37:12 -0700

    Execute Commands on Remote Server - remote-exec

--- Lab-18/main.tf
+++ Lab-18/main.tf
@@ -9,6 +9,7 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 resource "aws_instance" "myserver" {
   ami                    = "ami-0c9bfc21ac5bf10eb"
@@ -38,7 +39,8 @@ resource "aws_instance" "myserver" {
 
 
 resource "aws_security_group" "web" {
-  name = "My-SecurityGroup"
+  name   = "My-SecurityGroup"
+  vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
   ingress {
     from_port   = 22
     to_port     = 22
