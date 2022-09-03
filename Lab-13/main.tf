614271302e336cc4a4be2aabe08ecd7be1f02d84
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:36:09 -0700

    HighlyAvailable Web with Green/Blue Deployment

--- Lab-13/main.tf
+++ Lab-13/main.tf
@@ -14,6 +14,8 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 data "aws_availability_zones" "working" {}
 data "aws_ami" "latest_amazon_linux" {
   owners      = ["137112412989"]
@@ -25,7 +27,8 @@ data "aws_ami" "latest_amazon_linux" {
 }
 
 resource "aws_security_group" "web" {
-  name = "Web Security Group"
+  name   = "Web Security Group"
+  vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
   dynamic "ingress" {
     for_each = ["80", "443"]
     content {
