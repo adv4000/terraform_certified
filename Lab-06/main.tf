89f4de4124d605f5bc2cab7f8986cbe557b61ac9
Denis Astahov <adv4000@gmail.com>
2022-09-03 09:34:42 -0700

    Build WebServer with ElastiIP and Lifecycle

--- Lab-06/main.tf
+++ Lab-06/main.tf
@@ -9,6 +9,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 resource "aws_eip" "web" {
   instance = aws_instance.web.id
   tags = {
@@ -22,7 +24,7 @@ resource "aws_instance" "web" {
   instance_type               = "t3.micro"
   vpc_security_group_ids      = [aws_security_group.web.id]
   user_data                   = file("user_data.sh") // Static File
-  user_data_replace_on_change = true # Added in the new AWS provider!!!
+  user_data_replace_on_change = true                 # Added in the new AWS provider!!!
   tags = {
     Name  = "WebServer Built by Terraform"
     Owner = "Denis Astahov"
@@ -36,6 +38,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web" {
   name        = "WebServer-SG"
   description = "Security Group for my WebServer"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   dynamic "ingress" {
     for_each = ["80", "443"]
