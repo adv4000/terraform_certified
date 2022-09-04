7cf0566b6c166b526658ad98a9a08c0e877975cd
Denis Astahov <adv4000@gmail.com>
2022-09-04 11:54:16 -0700

    Use Global Variables via Module

--- Lab-35/stag/eip-stag.tf
+++ Lab-35/stag/eip-stag.tf
@@ -1,3 +1,3 @@
-resource "aws_eip" "stag-ip1" {}
-resource "aws_eip" "stag-ip2" {}
-resource "aws_eip" "stag-ip3" {}
+resource "aws_eip" "stag-ip1" { vpc = true } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "stag-ip2" { vpc = true } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "stag-ip3" { vpc = true } # Need to be added in new versions of AWS Provider
