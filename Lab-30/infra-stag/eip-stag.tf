e389e693b840874010fcac2eaa844ef0ee92d772
Denis Astahov <denis@astahov.net>
2023-07-16 22:29:19 -0700

    Update eip-stag.tf

--- Lab-30/infra-stag/eip-stag.tf
+++ Lab-30/infra-stag/eip-stag.tf
@@ -1,3 +1,3 @@
-resource "aws_eip" "stag-ip1" { vpc = true } # Need to be added in new versions of AWS Provider
-resource "aws_eip" "stag-ip2" { vpc = true } # Need to be added in new versions of AWS Provider
-resource "aws_eip" "stag-ip3" { vpc = true } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "stag-ip1" { domain = "vpc" } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "stag-ip2" { domain = "vpc" } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "stag-ip3" { domain = "vpc" } # Need to be added in new versions of AWS Provider
