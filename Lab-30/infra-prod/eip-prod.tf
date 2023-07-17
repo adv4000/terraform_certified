ae3e08adef60137aa4e516bdd2c2441b84472791
Denis Astahov <denis@astahov.net>
2023-07-16 22:29:01 -0700

    Update eip-prod.tf

--- Lab-30/infra-prod/eip-prod.tf
+++ Lab-30/infra-prod/eip-prod.tf
@@ -1,3 +1,3 @@
 
-resource "aws_eip" "prod-ip1" { vpc = true } # Need to be added in new versions of AWS Provider
-resource "aws_eip" "prod-ip2" { vpc = true } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "prod-ip1" { domain = "vpc" } # Need to be added in new versions of AWS Provider
+resource "aws_eip" "prod-ip2" { domain = "vpc" } # Need to be added in new versions of AWS Provider
